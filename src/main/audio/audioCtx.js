e034f23f79e918904538edb1975c7e9343764ac8
Nícolas Guedes <nicolastrocatti@gmail.com>
2026-03-09 22:41:56 -0300

    feat: updates the folder structure and changes the audio effect

--- src/main/audio/audioCtx.js
+++ src/main/audio/audioCtx.js
@@ -1,4 +1,6 @@
 export class AudioCtx {
+    isClicked = false;
+
     constructor(canvas) {
         this.canvas = canvas;
         this.addListeners();
@@ -6,27 +8,30 @@ export class AudioCtx {
 
     addListeners() {
         this.canvas.addEventListener("click", (e) => {
+            if (this.isClicked) return;
+            this.isClicked = true;
+
             this.ctx = new AudioContext();
             const osc = this.ctx.createOscillator();
             const gain = this.ctx.createGain();
 
-            osc.type = "square";
-            osc.frequency.setValueAtTime(130, this.ctx.currentTime);
-            osc.frequency.exponentialRampToValueAtTime(
-                196,
-                this.ctx.currentTime + 0.1
-            );
-            osc.frequency.exponentialRampToValueAtTime(
-                164,
-                this.ctx.currentTime + 0.2
+            osc.type = "sawtooth";
+            osc.frequency.setValueAtTime(294, this.ctx.currentTime);
+            osc.frequency.linearRampToValueAtTime(
+                340,
+                this.ctx.currentTime + 0.1,
             );
 
-            gain.gain.setValueAtTime(0.02, this.ctx.currentTime);
+            gain.gain.setValueAtTime(0.1, this.ctx.currentTime);
 
             osc.connect(gain).connect(this.ctx.destination);
             osc.start();
             osc.stop(this.ctx.currentTime + 0.2);
             this.ctx = null;
+
+            setTimeout(() => {
+                this.isClicked = false;
+            }, 200);
         });
     }
 }
---
+++ src/main/imports/assets.js
@@ -0,0 +1,31 @@
+const lightColor = "white";
+const darkColor = "blue";
+
+export const lightPawn = `/src/assets/chess/${lightColor}/pawn.png`;
+export const lightPieces = [
+    `/src/assets/chess/${lightColor}/rook.png`,
+    `/src/assets/chess/${lightColor}/knight.png`,
+    `/src/assets/chess/${lightColor}/bishop.png`,
+    `/src/assets/chess/${lightColor}/king.png`,
+    `/src/assets/chess/${lightColor}/queen.png`,
+    `/src/assets/chess/${lightColor}/bishop.png`,
+    `/src/assets/chess/${lightColor}/knight.png`,
+    `/src/assets/chess/${lightColor}/rook.png`,
+];
+
+export const darkPawn = `/src/assets/chess/${darkColor}/pawn.png`;
+export const darkPieces = [
+    `/src/assets/chess/${darkColor}/rook.png`,
+    `/src/assets/chess/${darkColor}/knight.png`,
+    `/src/assets/chess/${darkColor}/bishop.png`,
+    `/src/assets/chess/${darkColor}/queen.png`,
+    `/src/assets/chess/${darkColor}/king.png`,
+    `/src/assets/chess/${darkColor}/bishop.png`,
+    `/src/assets/chess/${darkColor}/knight.png`,
+    `/src/assets/chess/${darkColor}/rook.png`,
+];
+
+export const lightKing = `/src/assets/checkers/${lightColor}/king.png`;
+export const lightStone = `/src/assets/checkers/${lightColor}/stone.png`;
+export const darkKing = `/src/assets/checkers/${darkColor}/king.png`;
+export const darkStone = `/src/assets/checkers/${darkColor}/stone.png`;
